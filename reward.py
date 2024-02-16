9f958297d0ee465683a0a5e24b04f0bb2f99eed6
NGminecraft <99756151+NGminecraft@users.noreply.github.com>
2024-02-16 21:52:08 +0000

    Changes to be committed: 	modified:   reward.py

--- reward.py
+++ reward.py
@@ -59,10 +59,10 @@ class Reward(tf.keras.Model):
         return loss
 
     def init_train_data(self):
-        self.data_train = load_data("data\reward\training_data\data_train.csv")
+        self.data_train = load_data("data_train.csv")
 
     def init_train_test(self):
-        self.data_test = load_data("data\reward\training_data\data_test.csv")
+        self.data_test = load_data("data_test.csv")
 
     def build_model(self):
         activations = ["relu", "tanh", "sigmoid", "softmax", "softplus", "relu6", "softsign", "selu", "elu",
@@ -71,19 +71,21 @@ class Reward(tf.keras.Model):
                   "kl_divergence", "mean_squared_error", "mean_absolute_error", "mean_absolute_percentage_error",
                   "mean_squared_logarithmic_error", "cosine_similarity", "huber", "log_cosh", "hinge", "squared_hinge",
                   "categorical_hinge"]
+        
         for i in activations:
             self.model.add(keras.layers.Hashing(num_bins=len(self.data_train["Emotion"]), output_mode="one_hot"))
             self.model.add(keras.layers.Reshape((7934, 1)))
             self.model.add(keras.layers.Conv1D(64, 5, activation =i))
             self.model.add(keras.layers.Dense(5, activation =i))
             self.model.add(keras.layers.Dense(3, activation =i))
-            self.model.add(keras.layers.Dense(1, activation="tanh"))
+            self.model.add(keras.layers.Dense(1, activation=i))
             self.model.compile(loss='BinaryCrossentropy', optimizer='adam', metrics=["accuracy"])
             self.train()
+            print(self.model.evaluate(x=self.data_train["Text"].to_list(), y=self.data_train["Emotion"].to_list(), batch_size=100))
 
     def train(self):
         self.model.fit(x=self.data_train["Text"].to_list(), y=self.data_train["Emotion"].to_list(), batch_size=100,
-                       epochs=5,
+                       epochs=1,
                        validation_data=(self.data_test["Text"].to_list(), self.data_test["Emotion"].to_list()))
         keras.saving.save_model(self.model, "data//reward/models/trained.keras")
 
