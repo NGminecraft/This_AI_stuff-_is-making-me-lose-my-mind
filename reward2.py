17b64f24b2cc5c72e09e11ba1f49587b236de49a
NGminecraft <grahanic001@alpinesd.org>
2024-02-18 00:06:24 -0700

    Changes to be committed: 	new file:   reward2.py 	modified:   test.py

---
+++ reward2.py
@@ -0,0 +1,85 @@
+import keras
+import pandas as pd
+import os
+import tensorflow as tf
+
+
+def init_model():
+    if os.path.exists('data//reward//models//model.keras'):
+        return keras.saving.load_model('data//reward//models//model.keras'), True
+    else:
+        return keras.Sequential(), False
+
+
+def load_csv(csv):
+    return pd.read_csv(csv)
+
+
+def load_data(file):
+    data = load_csv(file)
+    words_to_numbers = {
+        "neutral": 0,
+        "sadness": -1,
+        "fear": -1,
+        "anger": -1,
+        "joy": 1
+    }
+    for i, v in enumerate(data["Emotion"]):
+        data.at[i, "Emotion"] = words_to_numbers[v]
+    return data
+
+
+class Reward:
+    def __init__(self):
+        self.tokenizer = keras.preprocessing.text.Tokenizer(num_words=1000,  filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True,  split=' ', char_level=False)
+        self.model, built = init_model()
+        self.init_train_data()
+        self.init_train_test()
+        if not built:
+            self.build_model()
+    
+    def init_train_data(self):
+        self.data_train = load_data("data_train.csv")
+
+    def init_train_test(self):
+        self.data_test = load_data("data_test.csv")
+    
+    def build_model(self):
+        word_input = keras.layers.Input(shape=(1,), name="Word_Input")
+        prev_word_input = keras.layers.Input(shape=(1,), name='prev_word_input')
+        additional_input = keras.layers.Input(shape=(1,), name='additional_input')
+        
+        vocabulary_size = 12940
+        embedding_dim = 200
+        word_embedding = keras.layers.Embedding(input_dim=vocabulary_size, output_dim=embedding_dim)(word_input)
+        
+        lstm_units = 150
+        lstm_layer = keras.layers.LSTM(units=lstm_units)(word_embedding)
+        
+        concatenated = keras.layers.Concatenate()([lstm_layer, prev_word_input, additional_input])
+        
+        dense_layer_1 = keras.layers.Dense(units=128, activation='relu')(concatenated)
+        dense_layer_2 = keras.layers.Dense(units=64, activation='relu')(dense_layer_1)
+        output = keras.layers.Dense(units=1, activation='tanh', name='output')(dense_layer_2)
+        
+        model = keras.Model(inputs=[word_input, prev_word_input, additional_input], outputs=output)
+        
+        model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+        
+        model.summary()
+        
+        train_data = pd.read_csv("data_train.csv")
+        test_data = pd.read_csv("data_test.csv")
+        tokenizer_sentences = []
+        for i in train_data["Text"]:
+            tokenizer_sentences.append(i) 
+        for i in test_data["Text"]:
+            tokenizer_sentences.append(i)
+        tokenizer = keras.preprocessing.text.Tokenizer()
+        tokenizer.fit_on_texts(tokenizer_sentences)
+        input_indices = [].append(tokenizer.word_index.get(i) for i in train_data)
+        
+        model.fit(x=[input_indices, 1, 1], y=train_data["Emotion"], validation_data=[].append(tokenizer.word_index.get(i) for i in test_data))
+        
+        
+Reward()
--- test.py
+++ test.py
@@ -1,8 +1,31 @@
-import tensorflow as tf
-import keras
+import pandas as pd
 
-a = keras.preprocessing.text.Tokenizer(num_words=10)
-a.fit_on_texts(["Hi Nick", "Hi Cows"])
-b = a.texts_to_sequences(["Hi Nick", "Hi Cows"])
-print(a.texts_to_sequences(["Hi Nick", "Hi Cows"]))
-print(b)
+def format(word):
+    word = word.lower()
+    item = []
+    for i in word:
+        if i.isnumeric() or (ord(i) >= 97 and ord(i) <=122):
+            item.append(i)
+    return ''.join(item)
+    
+    
+x = pd.read_csv("data_train.csv")
+result = []
+for i in x["Text"]:
+    i = i.split(' ')
+    for j in i:
+        j = format(j)
+        if j not in result:
+            result.append(j)
+        
+y = pd.read_csv("data_test.csv")
+for i in y["Text"]:
+    i = i.split(' ')
+    for j in i:
+        j = format(j)
+        if j not in result:
+            result.append(j)
+
+print(result)
+print()
+print(len(result))
