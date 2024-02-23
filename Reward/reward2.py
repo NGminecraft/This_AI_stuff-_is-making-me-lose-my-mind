3b2fe6b47591e7dea305989bdac369fb5aec85e5
NGminecrafter <minecrafterng399@gmail.com>
2024-02-23 07:18:47 -0700

    Its working

--- Reward/reward2.py
+++ Reward/reward2.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 import logging
 import copy
 import sys
+import matplotlib.pyplot as plt
 
 def format(word):
     pass
@@ -63,24 +64,23 @@ class Reward:
     def init_train_test(self):
         self.data_test = load_csv("Reward/Data/TrainingData/data_test.csv")
         
-    def _model_build(self):
-        word_input = keras.layers.Input(shape=(1500,), name="Word_Input")
-        prev_word_input = keras.layers.Input(shape=(1,), name='prev_word_input')
+    def _model_build(self, opt=keras.optimizers.Adam()):
+        word_input = keras.layers.Input(shape=(300,), name="Word_Input")
         additional_input = keras.layers.Input(shape=(1,), name='additional_input')
 
-        reshaped_input = keras.layers.Reshape((1, 1500))(word_input)
+        reshaped_input = keras.layers.Reshape((1, 300))(word_input)
 
-        lstm_layer = keras.layers.LSTM(units=1024, input_shape=(None, 1), activation='relu')(reshaped_input)
+        lstm_layer = keras.layers.LSTM(units=250, input_shape=(None, 1), activation='relu')(reshaped_input)
         
-        concatenated = keras.layers.Concatenate()([lstm_layer, prev_word_input, additional_input])
+        concatenated = keras.layers.Concatenate()([lstm_layer, additional_input])
         
-        dense_layer_1 = keras.layers.Dense(units=512, activation='linear')(concatenated)
-        dense_layer_2 = keras.layers.Dense(units=64, activation='linear')(dense_layer_1)
+        dense_layer_1 = keras.layers.Dense(units=200, activation='linear')(concatenated)
+        dense_layer_2 = keras.layers.Dense(units=100, activation='linear')(dense_layer_1)
         output = keras.layers.Dense(units=1, activation='tanh', name='output')(dense_layer_2)
         
-        model = keras.Model(inputs=[word_input, prev_word_input, additional_input], outputs=output)
+        model = keras.Model(inputs=[word_input, additional_input], outputs=output)
         
-        model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.008), loss='mse', metrics=['mean_squared_error'])
+        model.compile(optimizer=opt, loss='mse', metrics=['mean_squared_error'])
         
         self.logger.log(logging.INFO, 'model created')
         model.summary()
@@ -88,7 +88,7 @@ class Reward:
     
     def build_model(self):
 
-        if os.path.exists("Reward/Data/TrainingData/InputsandOutputs/inputs.npy") and os.path.exists("Reward/Data/TrainingData/InputsandOutputs/outputs.npy"):
+        if os.path.exists("Reward/Data/TrainingData/InputsandOutputs/inputs.npy") and os.path.exists("Reward/Data/TrainingData/InputsandOutputs/outputs.npy") and os.path.exists("Reward/Data/TrainingData/InputsandOutputs/validation_inputs.npy") and os.path.exists("Reward/Data/TrainingData/InputsandOutputs/validation_outputs.npy"):
             self.logger.log(logging.INFO, "Loading data from the npy files")
             input_indices = np.load("Reward/Data/TrainingData/InputsandOutputs/inputs.npy")
             self.logger.log(logging.INFO, f"Loaded input matrix of shape {input_indices.shape}")
@@ -99,7 +99,7 @@ class Reward:
             validation_output_indices = np.load("Reward/Data/TrainingData/InputsandOutputs/validation_outputs.npy")
             self.logger.log(logging.INFO, f"Loaded validation output matrix of shape {validation_output_indices.shape}")
         else:
-            self.logger.log(logging.INFO, "No data found, generating training data")
+            self.logger.log(logging.INFO, "Missing, regenerating training data")
             input_length = 300
 
             words_to_numbers = {
@@ -116,11 +116,10 @@ class Reward:
                 
             input_indices = np.array([0]*input_length)
             for i in tqdm(self.data_train["Text"], desc="Creating The Input Matrix"):
+                i = [i]
                 if sys.getsizeof(input_indices) >= 1000000000:
                     self.logger.log(logging.WARNING, f'Input matrix is larger then 1 GB. Size = {sys.getsizeof(input_indices)}')
-#                self.logger.log(logging.DEBUG,self.logger.log(logging.DEBUG, f"{len(self.tokenizer(i))} {self.tokenizer(i)}"))
-                input_indices = np.vstack((input_indices, self.padding(self.tokenizer(i), maxlen=input_length, padding='post', truncating='post')))
-            self.logger.log(logging.DEBUG, self._tokenizer.num_words)
+                input_indices = np.vstack((input_indices, self.padding(self.tokenizer(i), maxlen=input_length, padding='post', truncating='pre')))
             np.save("Reward/Data/TrainingData/InputsandOutputs/inputs", input_indices, allow_pickle=True)
             output_indices = np.array([[0]])
             for i in tqdm(self.data_train["Emotion"], desc="Creating The Output Matrix"):
@@ -130,6 +129,7 @@ class Reward:
 
             validation_input_indices = np.array([0]*input_length)
             for i in tqdm(self.data_test["Text"], desc="Creating The Validation Input Matrix"):
+                i = [i]
                 validation_input_indices = np.vstack((validation_input_indices, self.padding(self.tokenizer(i), maxlen=input_length, padding='post', truncating='post')))
             np.save("Reward/Data/TrainingData/InputsandOutputs/validation_inputs", validation_input_indices, allow_pickle=True)
             validation_output_indices = np.array([[0]])
@@ -138,13 +138,39 @@ class Reward:
             np.save("Reward/Data/TrainingData/InputsandOutputs/validation_outputs", validation_output_indices,
                     allow_pickle=True)
             
-        thingy = np.asarray([0]*input_indices.shape[0])
+        thingy = np.asarray([1]*input_indices.shape[0])
         
         self.logger.log(logging.INFO, f'Input loaded with shape {input_indices.shape} and looks like {input_indices}')
         self.logger.log(logging.INFO, f'Output loaded with shape {output_indices.shape} and looks like {output_indices}')
         self.logger.log(logging.INFO, f'Validation Input loaded with shape {validation_input_indices.shape} and looks like {validation_input_indices}')
         self.logger.log(logging.INFO, f'Validation Output loaded with shape {validation_output_indices.shape} and looks like {validation_output_indices}')
-        
-        model = self._model_build()
 
-        model.fit(x=[input_indices, thingy, thingy], y=output_indices, batch_size=10, epochs=20, validation_data=([validation_input_indices, np.asarray([0]*validation_input_indices.shape[0]), np.asarray([0]*validation_input_indices.shape[0])], validation_output_indices))
+        optimizers = [keras.optimizers.SGD(), keras.optimizers.RMSprop(), keras.optimizers.Adadelta(), keras.optimizers.Adagrad(), keras.optimizers.Adam(), keras.optimizers.Adamax(), keras.optimizers.AdamW()]
+
+        lowest_loss = 150
+        best_optimizer = None
+        best_history = None
+        all_histories = []
+        for i in optimizers:
+            all_histories.append([i])
+            model = self._model_build(opt = i)
+            history = model.fit(x=[input_indices, thingy], y=output_indices, batch_size=10, epochs=20, validation_data=([validation_input_indices, np.asarray([1]*validation_input_indices.shape[0])], validation_output_indices))
+            all_histories[-1].append(history)
+            self.logger.log(logging.INFO, f'Trained model with optimizer: {i}')
+            self.logger.log(logging.INFO, history)
+            if history.history['val_loss'][-1] < lowest_loss:
+                lowest_loss = history.history['val_loss'][-1]
+                best_optimizer = None
+                best_history = history
+                self.logger.log(logging.INFO, f'{i} is the new best optimizer')
+        x = np.arange(0, 20)
+        for i in all_histories:
+            history = i[1]
+            plt.plot(x, history.history['loss'], label=f"Loss of {i[0].name}")
+            plt.plot(x, history.history['val_loss'], label=f"Validation loss of {i[0].name}")
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.title('Model Comparison')
+        plt.legend()
+        plt.show()
+        plt.savefig('ModelComparison.png')
--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-from utils.Hasher.hasher import Hasher
-a = Hasher()
+import keras
 
-print(a.padding(["Hi", "Nick"], 5, 0))
-
+b= ["hello", "hi", "Nick", "Cow"]
+token = keras.preprocessing.text.Tokenizer(oov_token=-1, split=' ')
+token.fit_on_texts(b)
 while True:
-    print(a.hash_text(input("Enter text to hash: ")))
+    c = [input("Enter ")]
+    print(c)
+    b = token.texts_to_sequences(c)
+    print(b, "tokenized")
+    print(len(b), "length")
+    a = keras.preprocessing.sequence.pad_sequences(b, maxlen=5, padding='post', truncating='post')
+    print(a, "padded")
+    print(len(a), "length of padded")
+    print(a.shape, "shape of padded")
