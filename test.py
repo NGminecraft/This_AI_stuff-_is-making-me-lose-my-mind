824afa8a11c8790fd4f5b454fc1ef616760ee56f
NGminecrafter <minecrafterng399@gmail.com>
2024-02-26 21:37:29 -0700

    Final training

--- test.py
+++ test.py
@@ -9,4 +9,4 @@ b = keras.models.load_model('Reward/Data/Models/best_hp-number1')
 b.summary()
 """
 
-dirs = [for i in os.listdir('Reward/Data/Models/Training attempt 2') if not os.path.isfile(os.path.join('Reward/Data/Models/Training attempt 2'))]
+dirs = [for i in os.listdir('Reward/Data/Models/Training attempt 2') if not os.path.isfile(os.path.join('Reward/Data/Models/Training attempt 2', i))]
